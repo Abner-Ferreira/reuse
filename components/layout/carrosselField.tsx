590ad58217208dcd9648e2c2378e9023be6f7c23
Abner-Ferreira <abnerferr2015@gmail.com>
2026-03-05 15:32:06 -0300

    Componente de carrossel finalizado

--- components/layout/carrosselField.tsx
+++ components/layout/carrosselField.tsx
@@ -24,6 +24,12 @@ interface CarrosselProps {
 }
 
 export default function CarrosselField({ title, products }: CarrosselProps) {
+  const conservationColor: Record<string, string> = {
+    Novo: 'bg-green-500 hover:bg-green-600',
+    Seminovo: 'bg-yellow-500 hover:bg-yellow-600',
+    'Muito usado': 'bg-red-500 hover:bg-red-600',
+  }
+
   return (
     <>
       <h1 className='text-2xl text-(--text-gray) my-5'>{title}</h1>
@@ -41,19 +47,28 @@ export default function CarrosselField({ title, products }: CarrosselProps) {
                 key={index}
                 className='w-full sm:basis-1/2 md:basis-1/3 xl:basis-1/5'
               >
-                <Card className='relative mx-auto w-full max-w-sm pt-0'>
+                <Card className='relative mx-auto w-full max-w-sm pt-0 flex flex-col h-105'>
                   <div className='absolute inset-0 z-30 aspect-video' />
+                  <Badge
+                    className={`absolute self-end m-2 z-40 ${conservationColor[product.stateOfConservation] ?? 'bg-gray-500'}`}
+                  >
+                    {product.stateOfConservation}
+                  </Badge>
                   <img
                     src={product.images[0]}
                     alt='Event cover'
                     className='relative z-20 aspect-video w-full object-contain'
                   />
-                  <CardHeader>
+                  <CardHeader className='flex-1'>
                     <CardAction>
-                      <Badge variant={'secondary'}>{title}</Badge>
+                      <Badge variant={'secondary'}>{product.category}</Badge>
                     </CardAction>
-                    <CardTitle>{product.name}</CardTitle>
-                    <CardDescription>{product.description}</CardDescription>
+                    <CardTitle className='line-clamp-1' title={product.name}>
+                      {product.name}
+                    </CardTitle>
+                    <CardDescription className='line-clamp-2 overflow-hidden'>
+                      {product.description}
+                    </CardDescription>
                   </CardHeader>
                   <CardFooter>
                     <Button className='w-full'>Editar publicação</Button>
